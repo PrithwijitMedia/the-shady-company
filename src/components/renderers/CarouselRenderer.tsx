fbaf617bfd6f7fca88c15adea0d477a555caac16
Prithwijit <prithwijit3606@gmail.com>
2026-07-17 22:56:34 +0530

    carousel added

--- src/components/renderers/CarouselRenderer.tsx
+++ src/components/renderers/CarouselRenderer.tsx
@@ -3,188 +3,203 @@ import ProductCard from "@/components/site/ProductCard";
 import ProjectCard from "@/components/site/ProjectCard";
 
 import {
-  collection,
-  getDocs,
-  query,
-  where,
-  limit
+    collection,
+    getDocs,
+    query,
+    where,
+    limit
 } from "firebase/firestore";
 
 import { db } from "@/lib/firebase";
+import { getProjects } from "@/lib/projects";
+import { getCollections } from "@/lib/collections";
+import { getProducts } from "@/lib/products";
 
 export default async function CarouselRenderer({
-  block,
+    block,
 }: any) {
 
-  let ref = collection(
-    db,
-    block.source || "projects"
-  );
-
-  let q: any;
-
-  if (block.featuredOnly) {
-
-    q = query(
-      ref,
-      where(
-        "featured",
-        "==",
-        true
-      ),
-      limit(
-        block.limit || 8
-      )
+    let ref = collection(
+        db,
+        block.source || "projects"
     );
 
-  } else {
+    let q: any;
 
-    q = query(
-      ref,
-      limit(
-        block.limit || 8
-      )
-    );
+    if (block.featuredOnly) {
 
-  }
+        q = query(
+            ref,
+            where(
+                "featured",
+                "==",
+                true
+            ),
+            limit(
+                block.limit || 8
+            )
+        );
 
-  const snapshot =
-    await getDocs(q);
+    } else {
 
-  const items =
-    snapshot.docs.map(
-      doc => ({
-        id: doc.id,
-        ...doc.data()
-      })
-    );
+        q = query(
+            ref,
+            limit(
+                block.limit || 8
+            )
+        );
 
-  return (
+    }
 
-    <section
-      className="
+    const snapshot =
+        await getDocs(q);
+
+    /*const items =
+        snapshot.docs.map(
+            doc => ({
+                id: doc.id,
+                ...doc.data()
+            })
+        );
+*/
+const items =
+    block.source === "projects"
+        ? await getProjects()
+        : block.source === "collections"
+        ? await getCollections()
+        : await getProducts();
+    return (
+
+        <section
+            className="
         py-24
       "
-    >
+        >
 
-      <div
-        className="
+            <div
+                className="
           max-w-7xl
           mx-auto
           px-8
         "
-      >
+            >
 
-        <h2
-          className="
+                <h2
+                    className="
             text-5xl
             mb-4
           "
-        >
-          {block.title}
-        </h2>
+                >
+                    {block.title}
+                </h2>
 
-        {
+                {
 
-          block.subtitle && (
+                    block.subtitle && (
 
-            <p
-              className="
+                        <p
+                            className="
                 mb-10
                 text-neutral-500
               "
-            >
-              {block.subtitle}
-            </p>
+                        >
+                            {block.subtitle}
+                        </p>
 
-          )
+                    )
 
-        }
-
-        <div
-          className="
-           relative
-      min-h-screen
-      flex
-      items-end
-      h-full
-w-full
-lg:object-[center_35%]
-object-cover
-object-center
-md:object-center
-inset-0
-          
-          "
-        >
-
-          {
-
-            items.map(
-              (item: any) => (
+                }
 
                 <div
-                  key={item.id}
-                  className="
-                    min-w-[340px]
-                    snap-start
-                    flex-shrink-0
-                  "
+                    className="
+                    relative
+                    min-h-screen
+                    flex
+                    items-end
+                    h-full
+                    w-full
+                    lg:object-[center_35%]
+                    object-cover
+                    object-center
+                    md:object-center
+                    inset-0
+             flex
+            gap-8
+            overflow-x-auto
+            snap-x
+            snap-mandatory
+            pb-4
+                    "
                 >
 
-                  {
+                    {
 
-                    block.source ===
-                    "projects" && (
+                        items.map(
+                            (item: any) => (
 
-                      <ProjectCard
-                        project={item}
-                      />
+                                <div
+                                    key={item.id}
+                                    className="
+                    min-w-[340px]
+                     max-w-fit
+                    snap-start
+                    flex-shrink-0
+                  " 
+                       >
 
-                    )
+                                    {
 
-                  }
+                                        block.source ===
+                                        "projects" && (
 
-                  {
+                                            <ProjectCard
+                                                project={item}
+                                            />
 
-                    block.source ===
-                    "collections" && (
+                                        )
 
-                      <CollectionCard
-                        collection={item}
-                      />
+                                    }
 
-                    )
+                                    {
 
-                  }
+                                        block.source ===
+                                        "collections" && (
 
-                  {
+                                            <CollectionCard
+                                                collection={item}
+                                            />
 
-                    block.source ===
-                    "products" && (
+                                        )
 
-                      <ProductCard
-                        product={item}
-                      />
+                                    }
 
-                    )
+                                    {
 
-                  }
+                                        block.source ===
+                                        "products" && (
+
+                                            <ProductCard
+                                                product={item}
+                                            />
+
+                                        )
+
+                                    }
+
+                                </div>
+
+                            )
+
+                        )
+
+                    }
 
                 </div>
 
-              )
+            </div>
 
-            )
+        </section>
 
-          }
-
-        </div>
-
-      </div>
-
-    </section>
-
-  );
+    );
 
 }
